9c2430c54ca2829a8ff0de874f0d568a643ab976
함승훈(August) <conomeer@gmail.com>
2022-06-22 21:14:26 +0900

    build: Initialize project settings

---
+++ outstagram/src/main/kotlin/com/blackship/outstagram/OutstagramApiApplication.kt
@@ -0,0 +1,11 @@
+package com.blackship.outstagram
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OutstagramApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<OutstagramApiApplication>(*args)
+}
---
+++ outstagram/src/test/kotlin/com/blackship/outstagram/OutstagramApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.blackship.outstagram
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OutstagramApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
