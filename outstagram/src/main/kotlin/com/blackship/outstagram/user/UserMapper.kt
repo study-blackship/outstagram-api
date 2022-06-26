ed08311800881b87d1803b66ea4b179bab233205
bee <rkddbtjd123@gmail.com>
2022-06-26 19:47:58 +0900

    feat: initialized UserMapper object - mapping dto to domain object and vice versa

---
+++ outstagram/src/main/kotlin/com/blackship/outstagram/user/UserMapper.kt
@@ -0,0 +1,12 @@
+package com.blackship.outstagram.user
+
+import org.springframework.stereotype.Component
+
+@Component
+class UserMapper {
+
+    fun dtoToDomain(userDto: UserDto): User {
+        TODO()
+    }
+
+}
