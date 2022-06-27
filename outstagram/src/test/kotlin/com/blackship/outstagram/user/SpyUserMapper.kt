e87f16e140f905bac20aed4c5e2d6e65e29f3c37
bee <rkddbtjd123@gmail.com>
2022-06-27 17:55:07 +0900

    test: add SpyUserMapper.toDomainByResult field

--- outstagram/src/test/kotlin/com/blackship/outstagram/user/SpyUserMapper.kt
+++ outstagram/src/test/kotlin/com/blackship/outstagram/user/SpyUserMapper.kt
@@ -3,10 +3,11 @@ package com.blackship.outstagram.user
 class SpyUserMapper: UserMapper() {
 
     lateinit var toDomainByArguments: UserDto
+    var toDomainByResult = getUserDummy()
 
     override fun toDomainBy(userDto: UserDto): User {
         toDomainByArguments = userDto
-        return getUserDummy()
+        return toDomainByResult
     }
 
 }
