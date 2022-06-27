1a6994034c8ed3c83d1faab41a1311264db7b9ec
bee <rkddbtjd123@gmail.com>
2022-06-27 13:58:52 +0900

    test: getUserDtoDummy function initialization

--- outstagram/src/test/kotlin/com/blackship/outstagram/user/UserDummyProviderFunctions.kt
+++ outstagram/src/test/kotlin/com/blackship/outstagram/user/UserDummyProviderFunctions.kt
@@ -13,3 +13,17 @@ internal fun getUserDummy(
     _email = email,
     _profileImage = profileImage,
 )
+
+internal fun getUserDtoDummy(
+    id: Long? = 1L,
+    resourceServerId: String = "resourceServerId",
+    resourceServerName: String = "resourceServerName",
+    email: String = "email",
+    profileImage: String = "profileImage"
+) = UserDto(
+    id = id,
+    resourceServerId = resourceServerId,
+    resourceServerName = resourceServerName,
+    email = email,
+    profileImage = profileImage
+)
