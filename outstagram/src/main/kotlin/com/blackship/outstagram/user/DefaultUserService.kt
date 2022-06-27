bb084b29fbe926114c527ba947bcdbad2cbf9a92
bee <rkddbtjd123@gmail.com>
2022-06-27 17:57:08 +0900

    feat: add transactional annotation to DefaultUserService

--- outstagram/src/main/kotlin/com/blackship/outstagram/user/DefaultUserService.kt
+++ outstagram/src/main/kotlin/com/blackship/outstagram/user/DefaultUserService.kt
@@ -4,12 +4,12 @@ import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
+@Transactional
 class DefaultUserService(
     private val userRepository: UserRepository,
     private val userMapper: UserMapper
 ): UserService {
 
-    @Transactional
     override fun registerUser(userDto: UserDto) {
         val user = userMapper.toDomainBy(userDto)
         userRepository.save(user)
