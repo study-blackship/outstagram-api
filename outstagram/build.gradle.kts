625b2c5780b8a15df5da4834522d34cf4f7a93cd
bee <rkddbtjd123@gmail.com>
2022-07-03 01:19:20 +0900

    build: change database dependency to mariadb

--- outstagram/build.gradle.kts
+++ outstagram/build.gradle.kts
@@ -21,7 +21,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("com.h2database:h2")
+	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
