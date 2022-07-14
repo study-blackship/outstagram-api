f023c7a5907549229739e088aed7d45d1ab498a2
bee <rkddbtjd123@gmail.com>
2022-07-14 22:15:06 +0900

    build: add jwt dependency - add jwt configuration in application.yml

--- outstagram/build.gradle.kts
+++ outstagram/build.gradle.kts
@@ -18,6 +18,7 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+	implementation("io.jsonwebtoken:jjwt:0.9.1")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
